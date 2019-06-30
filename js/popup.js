5ce9e9f8f7775052d979c77c8b90386326feab39
Alex <49641838+Rogach36@users.noreply.github.com>
2019-06-30 16:55:04 +0400

    в скрипт добавил проверку на валидность полей формы, а также добавил свойство, позволяющее повторять тряску

--- js/popup.js
+++ js/popup.js
@@ -5,11 +5,15 @@ var form = popup.querySelector(".popup-form");
 var username = popup.querySelector(".field-name");
 var usermail = popup.querySelector(".field-mail");
 var message = popup.querySelector(".field-message");
+var storage = localStorage.getItem("username");
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("popup-show");
   username.focus();
+  if (storage) {
+    username.value = storage;
+  }
 });
 
 close.addEventListener("click", function(evt) {
@@ -20,20 +24,80 @@ close.addEventListener("click", function(evt) {
 
 
 form.addEventListener("submit", function(evt) {
-  if (!username.value || !usermail.value || !message.value)
+  if (!username.value || !usermail.value || !message.value) {
     evt.preventDefault();
-    console.log("Нужно ввести данные");
-  popup.classList.add("modal-error");
-  // username.classList.add("invalid");
-  //popup.offsetWidth = popup.offsetWidth;
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+} else {
+  localStorage.setItem("username", "username.value")
+}
 });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains("popup-show")) {
-      evt.preventDefault();
-      popup.classList.remove("popup-show");
-      popup.classList.remove("modal-error");
-    }
+form.addEventListener("submit", function(evt) {
+ if (!username.value) {
+ evt.preventDefault();
+ username.classList.add("invalid");
+ } else {
+   username.classList.remove("invalid");
+ }
+ });
+
+ form.addEventListener("submit", function(evt) {
+  if (!usermail.value) {
+  evt.preventDefault();
+  usermail.classList.add("invalid");
+  } else {
+    usermail.classList.remove("invalid");
   }
-});
+  });
+
+  form.addEventListener("submit", function(evt) {
+   if (!message.value) {
+   evt.preventDefault();
+   message.classList.add("invalid");
+   } else {
+     message.classList.remove("invalid");
+   }
+   });
+
+   window.addEventListener("keydown", function (evt) {
+     if (evt.keyCode === 27) {
+       if (popup.classList.contains("popup-show")) {
+         evt.preventDefault();
+         popup.classList.remove("popup-show");
+         popup.classList.remove("modal-error");
+       }
+     }
+   });
+
+//  form.addEventListener("submit", function(evt) {
+// if (!username.value || !usermail.value || !message.value) {
+//  evt.preventDefault();
+//  username.classList.add("invalid");
+// usermail.classList.add("invalid");
+// message.classList.add("invalid");
+// } else {
+//   popup.classList.remove("modal-error");
+//   username.classList.remove("invalid");
+//  usermail.classList.remove("invalid");
+//  message.classList.remove("invalid");
+// }
+// });
+//
+//
+
+//
+// form.addEventListener("submit", function(evt) {
+// if (!username.value || !usermail.value || !message.value) {
+// evt.preventDefault();
+// username.classList.add("invalid");
+// usermail.classList.add("invalid");
+// message.classList.add("invalid");
+// } else {
+//  popup.classList.remove("modal-error");
+//  username.classList.remove("invalid");
+// usermail.classList.remove("invalid");
+// message.classList.remove("invalid");
+// }
+// });
